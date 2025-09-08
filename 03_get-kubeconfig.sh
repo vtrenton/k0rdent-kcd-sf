8df33129203fda4b3ef6277b6e59fc2c7f979ffd
Trenton VanderWert <trenton.vanderwert@gmail.com>
2025-09-08 13:35:28 -0500

    get kubeconfig for all downstream clusters
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- 03_get-kubeconfig.sh
+++ 03_get-kubeconfig.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-kubectl -n kcm-system get secret my-gcp-cluster-kubeconfig -o jsonpath='{.data.value}' | base64 -d > downstream-kc.yaml
+kubectl -n kcm-system get secret gcp-cluster-kubeconfig -o jsonpath='{.data.value}' | base64 -d > gcloud-kc.yaml
+kubectl -n kcm-system get secret ec2-cluster-kubeconfig -o jsonpath='{.data.value}' | base64 -d > aws-kc.yaml
